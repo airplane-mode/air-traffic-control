6b2bae83f8f209d2688a8403773ceb376be666d7
Shane Gibbons <shane@bizbuzz.me>
2018-01-14 16:52:52 -0500

    start getting plumbing into place for a build and test process

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const Router = require("../build/air-traffic-control");
+
+describe("The air traffic control router", () => {
+
+  it("should be constructable", () => {
+    const router = new Router();
+  });
+
+});
