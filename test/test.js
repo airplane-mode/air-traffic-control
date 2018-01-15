248d34e10e47f190efb7d0df4eec2c3ab9fda32f
Shane Gibbons <shane@bizbuzz.me>
2018-01-15 13:00:33 -0500

    fix mocking of redux store

--- test/test.js
+++ test/test.js
@@ -2,10 +2,12 @@ const Router = require("../src/index");
 const redux = require("redux");
 const configureStore = require("redux-mock-store").default;
 
+const mockStore = configureStore([]);
+
 describe("The air traffic control router", () => {
 
   it("should be constructable", () => {
-    const store = configureStore({}, []);
+    const store = mockStore({}, []);
     const router = new Router(store);
   });
 
