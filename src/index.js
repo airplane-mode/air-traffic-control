cb9c06bb33e21ab07fbca8401c5064faf228e40a
Shane Gibbons <shane@bizbuzz.me>
2018-01-20 09:31:45 -0500

    use export syntax and let babel do the rest

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ if(typeof(Element) !== "undefined") {
   }
 }
 
-module.exports = class {
+export default class {
   /**
    * Creates a new router, given a reference to the redux store for action dispatch
    *
